34d5511ad85ecce57c1842b92ac6fc1bc19d2ff1
Daniyal <daniyaldehghan2021@gmail.com>
2024-09-13 16:39:21 +0330

    Create VendorModel in models/vendor_model.dart
    - Added VendorModel class to represent vendor data.
    - Included methods for converting VendorModel to and from JSON to facilitate interaction with MongoDB.

---
+++ lib/models/vendor_model.dart
@@ -0,0 +1,61 @@
+import 'dart:convert';
+
+class VendorModel {
+  // Defined the Fields thet we need
+  final String id;
+  final String fullName;
+  final String email;
+  final String state;
+  final String city;
+  final String locality;
+  final String role;
+  final String password;
+
+  VendorModel({
+    required this.id,
+    required this.fullName,
+    required this.email,
+    required this.state,
+    required this.city,
+    required this.locality,
+    required this.role,
+    required this.password,
+  });
+
+  /*
+    Convert to Map so that we can easily convert to json,
+    and this is because hat data will be set to mongodb in json 
+  */
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'fullName': fullName,
+      'email': email,
+      'state': state,
+      'city': city,
+      'locality': locality,
+      'role': role,
+      'password': password,
+    };
+  }
+
+  // Converting to Json, because the data will be sent with json
+  String toJson() => json.encode(toMap());
+
+  /*
+   Converting back to the vendor user object
+   so that we can make use of it within our application 
+  */
+  factory VendorModel.fromJson(Map<String, dynamic> map) {
+    return VendorModel(
+      id: map['_id'] as String,
+      fullName: map['fullName'] as String,
+      email: map['email'] as String,
+      state: map['state'] as String,
+      city: map['city'] as String,
+      locality: map['locality'] as String,
+      role: map['role'] as String,
+      password: map['password'] as String,
+    );
+  }
+}
