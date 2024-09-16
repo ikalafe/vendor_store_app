94f59580f7951fdc0e04949c62a540212d7586d1
Daniyal <daniyaldehghan2021@gmail.com>
2024-09-17 02:37:00 +0330

    Add BottomNavigationBar to MainVendorScreen for navigation
    - Implemented BottomNavigationBar with 5 items: درآمد, بارگزاری, ویرایش, سفارشات, خروج
    - Added dynamic page switching using onTap and currentIndex to track selected page
    - Customized appearance with selected and unselected item colors and a fixed background color
    - Adjusted BottomNavigationBar height based on screen size for better responsiveness

--- lib/views/screens/main_vendor_screen.dart
+++ lib/views/screens/main_vendor_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
 
 class MainVendorScreen extends StatefulWidget {
   const MainVendorScreen({super.key});
@@ -8,12 +9,38 @@ class MainVendorScreen extends StatefulWidget {
 }
 
 class _MainVendorScreenState extends State<MainVendorScreen> {
+  final Color selectedItemColor = const Color(0xff5796E4);
+  final Color unselectedItemColor = const Color(0xff0E0E0E);
+  int _pageIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'صفحه فروشنده',
+    return Scaffold(
+      bottomNavigationBar: SizedBox(
+        height: MediaQuery.of(context).size.height * 0.10,
+        child: BottomNavigationBar(
+          onTap: (value) {
+            setState(() {
+              _pageIndex = value;
+            });
+          },
+          currentIndex: _pageIndex,
+          selectedItemColor: selectedItemColor,
+          unselectedItemColor: unselectedItemColor,
+          backgroundColor: const Color(0xffEFF6FF),
+          type: BottomNavigationBarType.fixed,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Iconsax.dollar_square_copy), label: "درآمد"),
+            BottomNavigationBarItem(
+                icon: Icon(Iconsax.import_1_copy), label: "بارگزاری"),
+            BottomNavigationBarItem(
+                icon: Icon(Iconsax.edit_copy), label: "ویرایش"),
+            BottomNavigationBarItem(
+                icon: Icon(Iconsax.shopping_cart_copy), label: "سفارشات"),
+            BottomNavigationBarItem(
+                icon: Icon(Iconsax.logout_copy), label: "خروج"),
+          ],
         ),
       ),
     );
