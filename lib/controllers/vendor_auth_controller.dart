97324244e3687e968bbfdf9d5b510b7bcc8ddc3f
Daniyal <daniyaldehghan2021@gmail.com>
2024-09-16 22:52:31 +0330

    Store authentication token and vendor data in SharedPreferences after login
    - Added logic to store authentication token securely in SharedPreferences
    - Extracted vendor data from API response and updated the vendor state using Riverpod
    - Saved the vendor data as JSON in SharedPreferences for persistence
    - Ensured vendor state is set before navigating to the main page

--- lib/controllers/vendor_auth_controller.dart
+++ lib/controllers/vendor_auth_controller.dart
@@ -1,11 +1,16 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:vendor_store_app/global_variables.dart';
 import 'package:vendor_store_app/models/vendor_model.dart';
+import 'package:vendor_store_app/provider/vendor_provider.dart';
 import 'package:vendor_store_app/services/manage_http_response.dart';
 import 'package:vendor_store_app/views/screens/main_vendor_screen.dart';
+
+final providerContainer = ProviderContainer();
 
 class VendorAuthController {
   Future<void> signUpVendor({
@@ -76,7 +81,18 @@ class VendorAuthController {
       manageHttpResponse(
         response: response,
         context: context,
-        onSuccess: () {
+        onSuccess: () async {
+          SharedPreferences preferences = await SharedPreferences.getInstance();
+          // Extract the authentication token from the response body
+          String token = jsonDecode(response.body)['token'];
+          // Store the authentication token securely in SharedPreferences
+          preferences.setString('auth_token', token);
+          // Encode the user data recived from backend as json
+          final vendorJson = jsonEncode(jsonDecode(response.body)['vendor']);
+          // Update the application state with the vendor user data using riverpod
+          providerContainer.read(vendorProvider.notifier).setVendor(vendorJson);
+          // Store the data in SharedPreferences
+          await preferences.setString('vendor', vendorJson);
           Navigator.pushAndRemoveUntil(
               context,
               MaterialPageRoute(
