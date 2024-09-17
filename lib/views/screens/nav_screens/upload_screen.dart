adf73f24e9b6f15bea2181a048e68880af3b145c
Daniyal <daniyaldehghan2021@gmail.com>
2024-09-17 05:21:03 +0330

    Implement image selection and display functionality in UploadScreen
    - Added ImagePicker to allow users to pick images from the gallery
    - Created a list of File objects to store the selected images
    - Implemented chooseImage function to handle image selection and update the state
    - Added GridView to display selected images with an add button for uploading new images
    - Handled cases where no image is selected by showing a debug message
    - Integrated IconButton to trigger image selection

--- lib/views/screens/nav_screens/upload_screen.dart
+++ lib/views/screens/nav_screens/upload_screen.dart
@@ -1,10 +1,71 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class UploadScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:iconsax_flutter/iconsax_flutter.dart';
+import 'package:image_picker/image_picker.dart';
+
+class UploadScreen extends StatefulWidget {
   const UploadScreen({super.key});
 
   @override
+  State<UploadScreen> createState() => _UploadScreenState();
+}
+
+class _UploadScreenState extends State<UploadScreen> {
+  // Create an instance of ImagePicker to handle image selection
+  final ImagePicker picker = ImagePicker();
+
+  // Initialize an empty lsit to store the selected images
+  List<File> images = [];
+
+  // Define a function to choose image from the gallery
+  chooseImage() async {
+    // Use the picker to select an image from the gallery
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    // Check if no image was picked
+    if (pickedFile == null) {
+      debugPrint('No Image Picked');
+    } else {
+      // If an Image was Picked, Update the state and add the image to the list
+      setState(() {
+        images.add(File(pickedFile.path));
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(child:Text('Upload Screen'));
+    return Column(
+      children: [
+        GridView.builder(
+          shrinkWrap: true, // Allow the GridView to shrink to fit the content
+          itemCount: images.length +
+              1, // The number of items in the grid (+1 for the add button)
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 3,
+            crossAxisSpacing: 4,
+            mainAxisSpacing: 4,
+            childAspectRatio: 1,
+          ),
+          itemBuilder: (context, index) {
+            // If the index is 0, display an iconButton to add a new image
+            return index == 0
+                ? Center(
+                    child: IconButton(
+                      onPressed: () {
+                        chooseImage();
+                      },
+                      icon: const Icon(Iconsax.add_square_copy),
+                    ),
+                  )
+                : SizedBox(
+                    width: 50,
+                    height: 50,
+                    child: Image.file(images[index - 1]),
+                  );
+          },
+        )
+      ],
+    );
   }
 }
