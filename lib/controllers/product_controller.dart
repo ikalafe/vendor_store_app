d88157d14bf88684e0368e5205be19a4e00447c6
Daniyal <daniyaldehghan2021@gmail.com>
2024-09-21 05:20:13 +0330

    Implemented product data upload to database in product_controller.dart.
    - Created logic for sending product data to the database via POST request.
    - Integrated ProductModel for data serialization and HTTP request.
    - Added validation for category and subcategory fields.
    - Displayed success and error messages based on upload status.

--- lib/controllers/product_controller.dart
+++ lib/controllers/product_controller.dart
@@ -3,6 +3,10 @@ import 'dart:io';
 import 'package:cloudinary_public/cloudinary_public.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:vendor_store_app/global_variables.dart';
+import 'package:vendor_store_app/models/product_model.dart';
+import 'package:vendor_store_app/services/manage_http_response.dart';
+import 'package:http/http.dart' as http;
 
 class ProductController {
   void uploadProduct({
@@ -15,6 +19,7 @@ class ProductController {
     required String fullName,
     required String subCategory,
     required List<File>? pickedImages,
+    required context,
   }) async {
     if (pickedImages != null) {
       final cloudinary = CloudinaryPublic(
@@ -35,6 +40,50 @@ class ProductController {
         images.add(cloudinaryResponse.secureUrl);
         debugPrint('عکس محصول: $images');
       }
+      if (category.isNotEmpty && subCategory.isNotEmpty) {
+        final ProductModel productModel = ProductModel(
+          id: '',
+          productName: productName,
+          productPrice: productPrice,
+          quantity: quantity,
+          description: description,
+          category: category,
+          vendorId: vendorId,
+          fullName: fullName,
+          subCategory: subCategory,
+          images: images,
+        );
+        http.Response response = await http.post(
+          Uri.parse('$uri/api/add-product'),
+          body: productModel.toJson(),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+        );
+        manageHttpResponse(
+          response: response,
+          context: context,
+          onSuccess: () {
+            showSnackBar(
+              context,
+              'محصول با موفقیت بارگزاری شد.',
+              background: Colors.green,
+            );
+          },
+        );
+      } else {
+        showSnackBar(
+          context,
+          'دیته بندی یا زیر دسته بندی خالی است',
+          background: Colors.red,
+        );
+      }
+    } else {
+      showSnackBar(
+        context,
+        'عکس محصول را انتخواب کنید',
+        background: Colors.yellow.shade700,
+      );
     }
   }
 }
